8fa3a382b4a48a8837277132e2ab02d36cbdfb93
HesCoded <74415884+HesCoded@users.noreply.github.com>
2026-04-29 19:49:06 +0300

    Alternative main method added

---
+++ src/main/java/me/hescoded/devmangala/Starter.java
@@ -0,0 +1,9 @@
+package me.hescoded.devmangala;
+
+import javafx.application.Application;
+
+public class Starter {
+    public static void main(String[] args) {
+        Application.launch(DevMangala.class, args);
+    }
+}
